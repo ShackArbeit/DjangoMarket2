1ebe9f14111d73ebd14e2e9a28e8cae74da1f4dc
Shack <g0972222165@gmail.com>
2024-10-02 20:25:23 +0800

    Add DetailPage

--- item/views.py
+++ item/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Items
 
-# Create your views here.
+def detail(request,pk):
+      item=get_object_or_404(Items,pk=pk)
+       # 將尚未出售的同種類商品的前三項展示出來並排出當前的產品
+      related_items = Items.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+      return render(request,'item/detail.html',{
+            'item':item,
+            'related_items': related_items
+      })
+
