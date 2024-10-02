72c09a7dbef9f17998a6c651b81f4b439f18392f
Shack <g0972222165@gmail.com>
2024-10-02 13:25:44 +0800

    Add Index

--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from item.models import Items,Category
 
+# 將從 item app 裡面所定義的 Category,Items models 拿過來用
 def index(request):
-      return render(request,'./index.html')
+      items=Items.objects.filter(is_sold=False)[0:6]
+      categories=Category.objects.all()
+      return render(request,'./index.html',{
+            'categories':categories,
+            'items':items
+      })
 
 def contact(request):
       return render(request,'./contact.html')
--- item/models.py
+++ item/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+      name=models.CharField(max_length=200)
+
+      class Meta:
+        ordering = ('name',)
+        verbose_name_plural = '產品種類'
+      
+      def __str__(self):
+           return self.name
+
+class Items(models.Model):
+     name=models.CharField(max_length=200)
+     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+     description=models.TextField(blank=True,null=True)
+     price=models.FloatField()
+     image=models.ImageField(upload_to='item_images',blank=True,null=True)
+     is_sold=models.BooleanField(default=False)
+     created_by=models.ForeignKey(User,related_name='items',on_delete=models.CASCADE)
+     created_at=models.DateField(auto_now_add=True)
+     def __str__(self):
+        return self.name 
+
+     class Meta:
+        verbose_name_plural = '產品項目'
