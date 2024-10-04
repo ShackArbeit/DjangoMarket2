c55a73c1c006c0bbd94081ee5d9edb395ce33984
Shack <g0972222165@gmail.com>
2024-10-04 13:22:14 +0800

    AddItemView

--- item/forms.py
+++ item/forms.py
@@ -3,6 +3,7 @@ from .models import Items
 
 INPUT_CLASS='w-full py-4 px-6 rounded-xl border'
 
+# 新增產品的 Form 表單
 class NewItemForm(forms.ModelForm):
       class Meta:
             model=Items
@@ -26,3 +27,22 @@ class NewItemForm(forms.ModelForm):
                   }),
             }
 
+# 創建產品後的可修改產品的表單
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Items
+        fields = ('name', 'description', 'price', 'image', 'is_sold')
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASS
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASS
+            }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASS
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASS
+            })
+        }
--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Items
-from .forms import NewItemForm
+from .forms import NewItemForm,EditItemForm
 
 def detail(request,pk):
       item=get_object_or_404(Items,pk=pk)
@@ -31,9 +31,29 @@ def new(request):
         'title': '新增新產品',
     })
 
-
+# 需要註冊且登入才可以刪除產品
 @login_required
 def delete(request,pk):
     item=get_object_or_404(Items,pk=pk,created_by=request.user)
     item.delete()
     return redirect('dashboard:index')
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Items, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, './ItemForm.html', {
+        'form': form,
+        'title': '編輯修改產品',
+    })
